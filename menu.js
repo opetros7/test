45a8e61af6b8ae9d5e906f1f80949fd6d6bc9a56
opetros7 <xristopetros99@gmail.com>
2025-03-09 14:41:08 +0200

    menu.js

--- menu.js
+++ menu.js
@@ -12,9 +12,12 @@ function toggleMenu() {
     menu.classList.toggle('active');
 }
 
+
 document.addEventListener("DOMContentLoaded", function () {
     if (window.innerWidth <= 768) {
         let dropdowns = document.querySelectorAll(".dropdown-content");
-        dropdowns.forEach(dropdown => dropdown.remove()); // Διαγράφει τα dropdown στο κινητό
+        dropdowns.forEach(dropdown => {
+            dropdown.style.display = "none"; // Κρύβει τελείως το dropdown
+        });
     }
 });
